5ebc334f4d51e01f454e10513f888b2c67bc43b5
7sun <pakrouse@gmail.com>
2015-12-02 16:16:49 -0800

    configures with sendgrid account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,15 @@ Prelaunchr::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USER'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'steamd.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
